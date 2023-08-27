a44b7661dc995d59313c029423318ce4172a8bf0
Imamul5641 <97669936+Imamul5641@users.noreply.github.com>
2023-08-27 14:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// import apiKey from './config';
+import apiKey from './secrets';
+import access_Key from './secrets';
 const inputBox = document.querySelector('#place-input');
 const searchBtn = document.getElementById('searchBtn');
 const weather_img = document.querySelector('#weatherIcon');
